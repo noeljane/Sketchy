d0988348fe8825466e1a4620ca714765ca2c8c2e
Noel Jane Bourg <noeljanebourg@gmail.com>
2018-01-30 15:20:48 -0800

    added sketch model

---
+++ models/Sketch.js
@@ -0,0 +1,13 @@
+//Sketch Model here
+const
+    mongoose = require('mongoose'), 
+    sketchSchema = new mongoose.Schema({
+        imgUrl: String,
+        title: String,
+        keyword: String, 
+        _by:{type:mongoose.Schema.Types.ObjectId, ref: 'User'}
+
+    })
+
+const Sketch = mongoose.model('Sketch', sketchSchema)
+module.exports = Sketch
