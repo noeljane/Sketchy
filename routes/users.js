aa5f2d1aaea046c9ebe0f3d495eccac99e9f0a52
Kevin Wang <kw1017@gmail.com>
2018-01-30 12:30:06 -0800

    added userroutes

---
+++ routes/users.js
@@ -0,0 +1,39 @@
+// User routes
+const
+    express = require('express'),
+    passport = require('passport'),
+    userRouter = new express.Router()
+
+function isLoggedIn(req, res, next) {
+    if(req.isAuthenticated()) return next()
+        res.redirect('/login')
+}   
+
+userRouter.get('/login', (req, res) => {
+    res.render('login')
+})
+    
+userRouter.post('/login', passport.authenticate('local-login', {
+    successRedirect: '/profile',
+    failureRedirect: '/login'
+}))
+    
+userRouter.get('/signup', (req, res) => {
+    res.render('signup')
+})
+    
+userRouter.post('/signup', passport.authenticate('local-signup', {
+    successRedirect: '/profile',
+    failureRedirect: '/signup'
+}))
+    
+userRouter.get('/profile', isLoggedIn, (req, res) => {
+    res.render('profile', {user: req.user})
+})
+    
+userRouter.get('/logout', (req, res) => {
+    req.logout()
+    res.redirect('/')
+})
+    
+module.exports = userRouter
