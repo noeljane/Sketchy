f3b982bfb1c009b56f629759d7ec756884bde410
Noel Jane Bourg <noeljanebourg@gmail.com>
2018-01-30 10:27:37 -0800

    updated User model with b crypt

--- models/User.js
+++ models/User.js
@@ -1,1 +1,14 @@
 //User Model here
+const
+    mongoose = require('mongoose')
+    bcrypt = require('bcrypt-nodejs')
+    userSchema = new mongoose.Schema({
+        name: String, 
+        email: String, 
+        password: String
+    })
+
+//Create Password digest
+userSchema.methods.generateHash = function(password){
+    return bcrypt.hashSync(password,bcrypt.genSaltSync(8))
+}
