42d8ef737f4e0e6827b4ced874d12b0f0f080c1f
Kevin Wang <kw1017@gmail.com>
2018-01-30 11:02:44 -0800

    environment mongoose connectionmiddleware

--- server.js
+++ server.js
@@ -10,4 +10,30 @@ const
 	session = require('express-session'),
 	MongoDBStore = require('connect-mongodb-session')(session),
     passport = require('passport')
-    
+	
+	
+// Environment port
+const
+port = process.env.PORT || 3000,
+mongoConnectionString = process.env.MONGODB_URL || 'mongodb://localhost/sketchy'
+
+// Mongoose connection
+mongoose.connect(mongoConnectionString, (err) => {
+	console.log(err || "Connected to MongoDB!")
+})
+
+
+// Middleware
+app.use(logger('dev'))
+app.use(cookieParser())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(flash())
+
+app.get('/', (req, res) => {
+	res.send("working")
+})
+
+app.listen(port, (err) => {
+	console.log(err || "Server running on port " + port)
+})
