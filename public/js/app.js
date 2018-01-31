4596c73e9dde95bbd00b2c1b701d962ca490fd4c
Ishan <ishanliou@yahoo.com>
2018-01-31 09:31:35 -0800

    made canvas

--- public/js/app.js
+++ public/js/app.js
@@ -45,21 +45,115 @@ function displyImage(url){
     $imageContainer.html($img)
 }
 
-//user canvas
+$('#giphy-form').on('submit', function(evt) {
+    var imgUrl = $('#giphy-image-container').children('img').first().attr('src')
+    console.log(imgUrl)
+    $('#url-field').val(imgUrl)
+})
+
+
+//usesr canvas
 //variables
-var myCanvas = document.getElementById('myCanvas')
-var ctx = myCanvas.getContext('2d')
+var canvas;
+var ctx;
+var strokes = [];
+var currentStroke = null;
+var brush = {
+    x: 0,
+    y: 0,
+    color: '#000000',
+    size: 10,
+    down: false,
+};
 
-ctx.beginPath()
-ctx.arc(100, 100, 50, 1.5 * Math.PI, 0.5 * Math.PI, false)
-ctx.lineWidth = 15
-ctx.stroke()
+//functions
+function redraw () {
+    ctx.clearRect(0, 0, canvas.width(), canvas.height());
+    ctx.lineCap = 'round';
+    for (var i = 0; i < strokes.length; i++) {
+        var s = strokes[i];
+        ctx.strokeStyle = s.color;
+        ctx.lineWidth = s.size;
+        ctx.beginPath();
+        ctx.moveTo(s.points[0].x, s.points[0].y);
+        for (var j = 0; j < s.points.length; j++) {
+            var p = s.points[j];
+            ctx.lineTo(p.x, p.y);
+        }
+        ctx.stroke();
+    }
+}
 
-// var myImage = new Image()
-// myImage.src = imgData
-// $ctx.drawImage(myImage, 0, 0)
+function init () {
+    canvas = $('#myCanvas');
+    canvas.attr({
+        width: 600,
+        height: 600,
+    });
+    ctx = canvas[0].getContext('2d');
 
+    function mouseEvent (e) {
 
+        var rect = canvas[0].getBoundingClientRect();
+        brush.x = e.clientX - rect.left;
+        brush.y = e.clientY - rect.top;
 
-//Save the image
-//var imgData = $myCanvas.toDataURL()
+        console.log('brush x: ' + brush.x);
+
+        currentStroke.points.push({
+            x: brush.x,
+            y: brush.y,
+        });
+
+        redraw();
+    }
+
+    canvas.mousedown(function (e) {
+        brush.down = true;
+
+        currentStroke = {
+            color: brush.color,
+            size: brush.size,
+            points: [],
+        };
+
+        strokes.push(currentStroke);
+
+        mouseEvent(e);
+    }).mouseup(function (e) {
+        brush.down = false;
+
+        mouseEvent(e);
+
+        currentStroke = null;
+    }).mousemove(function (e) {
+        
+        if (brush.down)
+            mouseEvent(e);
+    });
+
+    $('#undo-btn').click(function () {
+        strokes.pop();
+        redraw();
+    });
+
+    $('#clear-btn').click(function () {
+        strokes = [];
+        redraw();
+    });
+
+    $('#color-picker').on('input', function () {
+        brush.color = this.value;
+    });
+
+    $('#save-btn').click(function () {
+        window.open(canvas[0].toDataURL());
+    });
+
+    $('#brush-size').on('input', function () {
+        brush.size = this.value;
+    });
+}
+
+$(init);
+
