dca4ee320cbb3611a51b690d55c08d8a7122b45c
Noel Jane Bourg <noeljanebourg@gmail.com>
2018-02-02 17:14:13 -0800

    commented user routes and sketch bug fix

--- routes/users.js
+++ routes/users.js
@@ -6,34 +6,40 @@ const
     Sketch = require('../models/Sketch.js')
     userRouter = new express.Router()
 
+// Function to check if logged in 
 function isLoggedIn(req, res, next) {
     if(req.isAuthenticated()) return next()
         res.redirect('/login')
 }   
-
+// Login in Route
 userRouter.get('/login', (req, res) => {
     res.render('login')
 })
-    
+
+//New Session Started 
 userRouter.post('/login', passport.authenticate('local-login', {
     successRedirect: '/profile', 
     failureRedirect: '/login'
 }))
-    
+
+// New User  
 userRouter.get('/signup', (req, res) => {
     res.render('signup')
 })
-    
+   
+// Create User
 userRouter.post('/signup', passport.authenticate('local-signup', {
     successRedirect: '/profile',
     failureRedirect: '/signup'
 }))
-    
+
+// Profile view
 userRouter.get('/profile', isLoggedIn, (req, res) => {
     // res.render('profile', {user: req.user})
     res.redirect(`/users/${req.user._id}`)
 })
-    
+
+// Log out route (deletes session)
 userRouter.get('/logout', (req, res) => {
     req.logout()
     res.redirect('/')
@@ -66,15 +72,6 @@ userRouter.get('/users/:id', isLoggedIn, (req, res) => {
     })
 })
 
-
-// Delete user
-userRouter.delete('/users/:id', isLoggedIn, (req,res) => {
-    User.findByIdAndRemove(req.params.id, (err, deletedUser) => {
-        if(err) return console.log(err)
-        res.redirect('/logout')
-    })
-})
-
 // Get edit user view
 userRouter.get('/profile/edit', isLoggedIn, (req, res) => {
     res.render('users_views/edituser', {user: req.user})
@@ -87,6 +84,17 @@ userRouter.patch('/users/:id', isLoggedIn, (req, res) => {
         res.redirect('/profile')
     })
 })
+
+
+// Delete user
+userRouter.delete('/users/:id', isLoggedIn, (req,res) => {
+    User.findByIdAndRemove(req.params.id, (err, deletedUser) => {
+        if(err) return console.log(err)
+        res.redirect('/logout')
+    })
+})
+
+
 
 
 module.exports = userRouter
